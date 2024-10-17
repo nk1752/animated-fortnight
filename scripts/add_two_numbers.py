e421f3202733cfb8c860d7c449eb53c2443c6819
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-17 14:32:54 -0500

    aaa

--- scripts/add_two_numbers.py
+++ scripts/add_two_numbers.py
@@ -1,7 +1,13 @@
 import sys
 import os
 
-def add_two_numbers(a, b)->int:
+def add_two_numbers()->int:
+
+  a = os.getenv('num1')
+  b = os.getenv('num2')
+  
   sum = a + b
   print( f"a + b = sum" )
+  
+  os.setenv('sum', sum)
   return sum
