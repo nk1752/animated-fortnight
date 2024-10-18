148467ea82d1b6dc727ab1f3bd212160e65acf60
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-18 12:12:57 -0500

    try2

--- scripts/display_json.py
+++ scripts/display_json.py
@@ -5,10 +5,13 @@ def display_json():
 
   # load config.json
   with open('config.json') as fh:
-    config = json.load(fh)
+    config_dict = json.load(fh)
 
   # print config.json
-  print(f"config: {config}")
+  print(f"config: {config_dict}")
 
   with open(os.getenv('GITHUB_OUTPUT'), 'a') as fh:
-    print(f"result={config['RegionsBank']['DEV']}", file=fh)
+    print(f"result={config_dict['RegionsBank']['DEV']}", file=fh)
+
+if __name__ == '__main__':
+    display_json()
