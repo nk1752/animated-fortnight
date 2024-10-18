a75af710c5001f173d41d36a9f5ddc4a28ac346f
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-18 13:17:23 -0500

    api key

--- scripts/weather.py
+++ scripts/weather.py
@@ -5,7 +5,7 @@ def weather():
   city = os.getenv('CITY')
   print(f"city: {city}")
 
-  API_key = os.getenv('API_kEY')
+  API_key = os.getenv('API_KEY')
   print(f"API_key: {API_key}")
 
   url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}'
