9cfa54420f65b5d925cf1cca6aefa796c74084fd
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-19 07:05:30 -0500

    postman

--- scripts/postman.py
+++ scripts/postman.py
@@ -2,25 +2,30 @@ import os
 import requests
 import json
 
+
 def postman():
-  print("Postman Echo function")
-  
-  
-  url = 'https://postman-echo.com/get?foo1=bar1&foo2=bar2'
-  response = requests.get(url)
-  
-  if response.status_code == 200:
-    print("Success")
-    data_json = response.json()
-    print(data_json)
-    data_dict = json.loads(data_json)
-    print(data_dict)
-  else:
-    print("Failed")
-    
+    print("Postman Echo function")
 
-  # with open(os.getenv('GITHUB_OUTPUT'), 'a') as fh:
-  #   print(f"result=Weather in {city} is 75F", file=fh)
+    url = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
+    response = requests.get(url)
 
-if __name__ == '__main__':
+    if response.status_code == 200:
+        print("Success")
+
+        print(f"Response: {response}")
+
+        data_json = response.json()
+        print(data_json)
+
+        data_dict = json.loads(data_json)
+        print(data_dict)
+
+    else:
+        print("Failed")
+
+    # with open(os.getenv('GITHUB_OUTPUT'), 'a') as fh:
+    #   print(f"result=Weather in {city} is 75F", file=fh)
+
+
+if __name__ == "__main__":
     postman()
