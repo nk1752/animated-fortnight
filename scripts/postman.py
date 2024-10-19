a3d3b912558db95420d3f8216a590cbea2fbcdeb
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-19 06:53:05 -0500

    display

--- scripts/postman.py
+++ scripts/postman.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 
 def postman():
   print("Postman Echo function")
@@ -7,8 +8,13 @@ def postman():
   
   url = 'https://postman-echo.com/get?foo1=bar1&foo2=bar2'
   response = requests.get(url)
+  
   if response.status_code == 200:
     print("Success")
+    data_json = response.json()
+    print(data_json)
+    data_dict = json.loads(data_json)
+    print(data_dict)
   else:
     print("Failed")
     
