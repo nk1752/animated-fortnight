f7dbccce7893ee87576f4c2e234eecafad78ec48
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-20 16:11:20 -0500

    dlkfjglfdjglfdkjglfdkj

--- scripts/bg_module.py
+++ scripts/bg_module.py
@@ -3,7 +3,7 @@ import json
 import yaml
 
 
-def bg_json(bg: str) -> int:
+def bg_json(bg) -> int:
     print("bg json function")
 
     print(f"bg -> {bg}")
@@ -25,24 +25,3 @@ def bg_json(bg: str) -> int:
             env_id = value['DEV']
             return 200
     return 0
-        
-def bg_json(bg):
-    print("bg json function")
-
-    print(f"bg -> {bg}")
-
-    # Open the YAML file and load it into a Python dictionary
-    with open('config.yaml', 'r') as file:
-        config_dict = yaml.load(file, Loader=yaml.FullLoader)
-    
-
-    print('key, value pairs of dictionary')
-    for key, value in config_dict.items():
-        print(f'key: {key} --> value: {value}')
-
-    print(f'find bg value in dictionary & get env_id from DEV')
-    env_id = None
-    for key, value in config_dict.items():
-        if key == bg:
-            env_id = value['DEV']
-            break
--- scripts/main.py
+++ scripts/main.py
@@ -2,10 +2,13 @@
 import os
 
 from my_module import my_function
+from bg_module import bg_json
 
 def main():
 
   my_function()  # Output: Hello from my function!
+  status = bg_json('BG')  # Output: bg json function
+  print(f"BG env_id: {status}")  # Output: BG env_id: 200
 
 if __name__ == '__main__':
     main()
