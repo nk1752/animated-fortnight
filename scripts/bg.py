5e1fc0a8dd375a1d6dce76d118b933d0754d8492
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-20 15:36:34 -0500

    bg entry pt

--- scripts/bg.py
+++ scripts/bg.py
@@ -2,13 +2,11 @@ import os
 
 # import functions from bg_json.py and bg_yaml.py
 from bg_module import bg_json
-from bg_module import bg_yaml
 
-def bg():
-  bg = os.getenv("BG")
 
-  bg_json(bg)
-  bg_yaml(bg)
+bg = os.getenv("BG")
+
+bg_json(bg)
 
 if __name__ == '__main__':
   bg()
