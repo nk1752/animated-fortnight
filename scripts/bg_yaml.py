32386c2c0f5e6758a0ced4faffd6187fff53fe02
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-20 14:57:46 -0500

    try yaml

--- scripts/bg_yaml.py
+++ scripts/bg_yaml.py
@@ -1,28 +1,32 @@
 import os
-import json
+import yaml
 
-def bg_yaml() -> int:
-    print("bg yaml function")
+def bg_json() -> int:
+    print("bg json function")
+
+    # get bg value from env
+    bg = os.getenv("BG")
+    print(f"bg -> {bg}")
 
     # Open the JSON file and load it into a Python dictionary
     with open('config.json', 'r') as file:
-        config_dict = json.load(file)
+        config_dict = yaml.load(file)
 
-    # Print the dictionary
-    #print(config_dict)
-    bg = 'RegionsBank'
-
-    # print all keys and values
+    print('key, value pairs of dictionary')
     for key, value in config_dict.items():
-        print(key, value["DEV"])
+        print(f'key: {key} --> value: {value}')
 
-    # print RegionsBank DEV value
-    env_id = config_dict[{bg}]['DEV']
-    print(f"env_id -> {env_id}")
+    print(f'find bg value in dictionary & get env_id from DEV')
+    env_id = None
+    for key, value in config_dict.items():
+        if key == bg:
+            env_id = value['DEV']
+            break
     
-    # step values
+    
+    print(f'step values')
     with open(os.getenv("GITHUB_OUTPUT"), "a") as fh:
         print(f"env_id={env_id}", file=fh)
 
 if __name__ == '__main__':
-    bg_yaml()
+    bg_json()
