ee5bb2a1f0e26c55ac9a207935182fa9b04f28b9
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-20 16:04:27 -0500

    main

--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,11 @@
 # This is a simple python script that adds two numbers and returns the sum.
 import os
 
+from my_module import my_function
+
 def main():
-  num1 = os.getenv('num1')
-  num2 = os.getenv('num2')
-  
-  print(f"num 1: {num1}")
-  print(f"num 2: {num2}")
 
-  sum = int(num1) + int(num2)
-  print(f"Sum: {sum}")
-
-  with open(os.getenv('GITHUB_OUTPUT'), 'a') as fh:
-    print(f"result={sum}", file=fh)
+  my_function()  # Output: Hello from my function!
 
 if __name__ == '__main__':
     main()
