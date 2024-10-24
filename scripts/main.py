13cbbce69545429bb3b7963e29030a55f2f1fd2c
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-24 15:37:47 -0500

    data.json

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,14 @@
 # This is a simple python script that adds two numbers and returns the sum.
 import os
 
-from my_module import my_function
+from dict import load_dict
 from bg_module import bg_json
 
 def main():
 
-  my_function()  # Output: Hello from my function!
-  status = bg_json('BG')  # Output: bg json function
-  print(f"BG env_id: {status}")  # Output: BG env_id: 200
+  bg_name = os.environ.get('BG_NAME')
+
+  load_dict(bg_name)
 
 if __name__ == '__main__':
     main()
