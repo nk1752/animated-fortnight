7ac76c2fe4b3768991631f7488d8e4aba46b8c25
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-27 18:00:07 -0500

    test

--- scripts/main.py
+++ scripts/main.py
@@ -5,21 +5,21 @@ import json
 from dict import load_dict
 from bg_module import bg_json
 
+
 def main():
 
-  bg_name = os.environ.get('BG_NAME')
+    bg_name = os.environ.get("BG_NAME")
 
-  fgw_dict = load_dict(bg_name)
+    fgw_dict = load_dict(bg_name)
 
-   # convert the fgw_dict to json
-  json_data = json.dumps(fgw_dict, indent=4)
-  print(json_data)
+    # convert the fgw_dict to json
+    json_data = json.dumps(fgw_dict, indent=4)
+    print(json_data)
 
-  # write the json data to a file
-  with open('output.json', 'w') as file:
-    file.write(json_data)
+    # write the json data to a file
+    with open("output.json", "w") as file:
+        file.write(json_data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-  
-  
